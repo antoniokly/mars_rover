49e8f2b1db315ab44c209ee793011b2ec63e25e6
Antonio Yip <antoniokly@icloud.com>
2019-05-05 20:58:48 +1200

    Site class

---
+++ Mars Rover/Mars Rover/Model/Site.swift
@@ -0,0 +1,20 @@
+//
+//  Site.swift
+//  Mars Rover
+//
+//  Created by Antonio Yip on 5/05/19.
+//
+
+import Foundation
+
+class Site {
+    var name: String
+    var grid: Coordinate
+    var rovers: [Rover]
+    
+    init(name: String, grid: Coordinate, rovers: [Rover] ) {
+        self.name = name
+        self.grid = grid
+        self.rovers = rovers
+    }
+}
