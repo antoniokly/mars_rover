f18c3bdb17d6f025cb3b9a24cbbb6ed23c7519d0
Antonio Yip <antoniokly@icloud.com>
2019-05-05 19:09:05 +1200

    xcode project

---
+++ Mars Rover/Mars Rover/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Mars Rover
+//
+//  Created by Antonio Yip on 5/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
