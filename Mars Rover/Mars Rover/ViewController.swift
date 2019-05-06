a25e407a5e36ce9e3514f390ec6b37fd3838eff6
Antonio Yip <antoniokly@icloud.com>
2019-05-06 20:57:01 +1200

    add rover

--- Mars Rover/Mars Rover/ViewController.swift
+++ Mars Rover/Mars Rover/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var gridView: UIView!
    
     @IBAction func addButtonTapped(_ sender: Any) {
+        let name = "Rover \(site.rovers.count + 1)"
         
+        let rover = Rover(name: name, position: Position(coordinate: Coordinate(x: 0,y: 0), heading: .E))
+        
+        site.rovers.append(rover)
+        selectedRover = rover
     }
-    
     
     @IBAction func moveButtonTapped(_ sender: Any) {
         
@@ -24,10 +28,6 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rightButtonTapped(_ sender: Any) {
-    }
-    
-    
-    @IBAction func addButtonTapped(_ sender: Any) {
     }
     
     @IBAction func resetButtonTapped(_ sender: Any) {
@@ -42,9 +42,19 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var statusLabel: UILabel!
     
+    var site: Site!
+    
+    var selectedRover: Rover? {
+        didSet {
+            updateStatus()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        site = Site(name: "Mars", grid: Coordinate(x: Int(view.bounds.width / 10.0), y: Int(view.bounds.height / 10.0)), rovers: [])
+        updateStatus()
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,6 +63,16 @@ class ViewController: UIViewController {
     }
 
     
+    func updateStatus() {
+        if let rover = selectedRover {
+            statusLabel.text = """
+            Name: \(rover.name)
+            Position: \(rover.finalPosition.string)
+            """
+        } else {
+            statusLabel.text = nil
+        }
+    }
 
 }
 
