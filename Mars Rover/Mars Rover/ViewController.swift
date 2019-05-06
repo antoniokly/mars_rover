d4cf4cd66a9a66abddb0f682dfa752f9d0f619fd
Antonio Yip <antoniokly@icloud.com>
2019-05-06 20:36:05 +1200

    view design

--- Mars Rover/Mars Rover/ViewController.swift
+++ Mars Rover/Mars Rover/ViewController.swift
@@ -8,7 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var gridView: UIView!
+   
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func moveButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func leftButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func rightButtonTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func undoButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func replayButtonTapped(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +52,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
