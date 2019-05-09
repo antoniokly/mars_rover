6a02a0771f22eeb6185c31d75e98e29a80fdf891
Antonio Yip <antonio.yip@asb.co.nz>
2019-05-10 10:35:32 +1200

    instructions

--- Mars Rover/Mars Rover/Controller/CommandInputViewController.swift
+++ Mars Rover/Mars Rover/Controller/CommandInputViewController.swift
@@ -43,6 +43,27 @@ class CommandInputViewController: UIViewController {
         }
     }
     
+    @IBAction func instructionsButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Instructions", message: nil, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.left
+        
+        let messageText = NSMutableAttributedString(
+            string: instructions,
+            attributes: [
+                NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                NSAttributedStringKey.font: UIFont.systemFont(ofSize: 12.0)
+            ]
+        )
+        
+        alert.setValue(messageText, forKey: "attributedMessage")
+        
+        present(alert, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -51,3 +72,19 @@ class CommandInputViewController: UIViewController {
         inputTextView.text = mainVC.site.commandString
     }
 }
+
+let instructions =
+"""
+The possible letters are 'N', 'E', 'S', 'W' (representing the rover's orientation), 'L', 'R' and 'M'. 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot. 'M' means move forward one grid point, and maintain the same heading.
+
+The first line of input is the upper-right coordinates of the plateau, the lower-left coordinates are assumed to be 0,0.
+The rest of the input is information pertaining to the rovers that have been deployed. Each rover has two lines of input. The first line gives the rover's position, and the second line is a series of instructions telling the rover how to explore the plateau. The position is made up of two integers and a letter separated by spaces, corresponding to the x and y co-ordinates and the rover's orientation.
+Each rover will be finished sequentially, which means that the second rover won't start to move until the first one has finished moving.
+
+Example:
+5 5
+1 2 N
+LMLMLMLMM
+3 3 E
+MMRMMRMRRM
+"""
